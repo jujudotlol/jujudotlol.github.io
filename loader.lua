df74097dad08c4d94c809ede62c71cb3a9273dcc
jujudotlol <160071511+jujudotlol@users.noreply.github.com>
2024-06-11 00:49:58 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,22 @@
 local is_solara = string.find(identifyexecutor(), "Solara")
-local games = {
+local games = is_solara and {
+    [4348829796] = "https://api.luarmor.net/files/v3/loaders/ca362c86261381fdb860a6cf99f6ab0c.lua",
+    [1008451066] = "https://api.luarmor.net/files/v3/loaders/4749f2f291e3f60f34f3c8f1ef7ef33e.lua",
+} or {
     [1008451066] = "https://api.luarmor.net/files/v3/loaders/f6a815eb5fa24e03f6a21e00582072b1.lua",
     [4348829796] = "https://api.luarmor.net/files/v3/loaders/83afa5fcd967550d4124c1ac22e66185.lua",
     [113491250] = "https://api.luarmor.net/files/v3/loaders/dc7209296a81c2e8684daa5cf40d6c44.lua"
 }
 
 if is_solara then
-    if game.GameId ~= 1008451066 then
-        game:GetService("Players").LocalPlayer:Kick("juju.lol > Solara only supports the Da Hood script!")
+    if not games[game.GameId] then
+        game:GetService("Players").LocalPlayer:Kick("juju.lol > Solara does not support this game!")
         return 
     end
 
     getgenv().script_key = script_key
     
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4749f2f291e3f60f34f3c8f1ef7ef33e.lua"))()
+    loadstring(game:HttpGet(games[game.GameId]))()
     return
 end
 
