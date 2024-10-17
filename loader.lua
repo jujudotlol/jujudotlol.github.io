2a1f9bf9145b587bd2ba608a913e87b0ddcc28d6
jujudotlol <160071511+jujudotlol@users.noreply.github.com>
2024-10-17 10:33:57 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -17,8 +17,9 @@ if game_id == 113491250 then
     local fflag = getfflag("DebugRunParallelLuaOnMainThread") 
     if not fflag or fflag == "false" then
         game:GetService("Players").LocalPlayer:Kick("juju > rejoining server! the script will execute afterwards :)")
-        setfflag("DebugRunParallelLuaOnMainThread", true)
-        queue_on_teleport(`task.wait(5)\n script_key = "{script_key}"\n loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/02f0f396b66a5e40edcee7552d8c24a6.lua"))()`)
+        setfflag("DebugRunParallelLuaOnMainThread", "true")
+        queue_on_teleport(`repeat task.wait() until game:IsLoaded(); task.wait(5)\n script_key = "{script_key}"\n loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/02f0f396b66a5e40edcee7552d8c24a6.lua"))()`)
+        task.wait(1)
         game:GetService("TeleportService"):Teleport(game.PlaceId)
     else
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/02f0f396b66a5e40edcee7552d8c24a6.lua"))()
