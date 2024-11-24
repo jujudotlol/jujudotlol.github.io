a55843da6547f61245bd6cd5706b612348c7960f
jujudotlol <160071511+jujudotlol@users.noreply.github.com>
2024-11-24 17:27:43 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,4 +9,4 @@ if is_solara then
     getgenv().script_key = script_key
 end
 
-loadstring(game:HttpGet(games[game_id] or "https://api.luarmor.net/files/v3/loaders/e7b8ab8480015787078f991bc7523cb6.lua"))()
+loadstring(game:HttpGet(games[game_id] or "https://api.luarmor.net/files/v3/loaders/f5cfd7f131473a4b567caed767725cde.lua"))()
